44bb8f9a86abcfcd44e5a7ec2590df0cce4a6070
Addi-11 <saddi.5131@gmail.com>
2019-09-11 04:07:35 +0530

    circle drawing algo

---
+++ bcircle.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<GL/glut.h>
+
+#include"setup.h"
+
+//BRESENHAM CIRCLE DRAWING ALGORITHM
+
+void drawCircle(int xc, int yc, int x, int y){
+    setPixel(xc + x , yc + y);
+    setPixel(xc - x , yc + y);
+    setPixel(xc + x , yc - y);
+    setPixel(xc - x , yc - y);
+    setPixel(xc + y , yc + x);
+    setPixel(xc - y , yc + x);
+    setPixel(xc + y , yc - x);
+    setPixel(xc - y , yc - x);
+}
+
+void circleBres(int xc, int yc, int r){
+    int x = 0, y = r;
+    int d = 3 - 2*r;
+    drawCircle(xc, yc, x, y);
+    while( y >= x){
+        x++;
+        if(d > 0){
+            y--;
+            d += 4*(x-y) + 10;
+        }
+        else{
+            d += 4*x + 6;
+        }
+        drawCircle(xc,yc,x,y);
+    }
+}
+
+void draw(){
+    circleBres(250,-100,200);
+    glFlush();
+}
